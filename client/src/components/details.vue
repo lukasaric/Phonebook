84ed9592b069dabbd5f7510f63687985ac46639b
Luka Šarić <lsaric00@fesb.hr>
2019-01-11 19:43:39 +0100

    Configured details container width

--- client/src/components/details.vue
+++ client/src/components/details.vue
@@ -1,56 +1,54 @@
 <template>
-  <v-layout class="mainContainer">
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card class="elevation-12">
-        <v-toolbar flat dark color="#07889B">
-          <v-toolbar-title class="tableTitle"> Contact details </v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-layout row>
-            <v-flex xs7>
-              <v-list-tile-content class="infoContainer">
-                <v-list-tile-title>
-                  <v-icon>person</v-icon>
-                  {{ contact.firstName }} {{ contact.lastName }}
-                </v-list-tile-title>
-                <v-list-tile-sub-title class="emailBar">
-                  <v-icon>fa fa-envelope</v-icon>
-                  {{ contact.email }}
-                </v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-flex>
-          </v-layout>
-          <v-divider light></v-divider>
-          <v-list two-line>
-            <v-list-tile-content class="primaryNumberContainer">
-              <v-list-tile-title> Primary number: </v-list-tile-title>
-              <v-list-tile-sub-title>
-                <v-icon color="orange">far fa-star</v-icon>
-                {{ contact.primaryNumber.number }}
+  <div class="mainContainer">
+    <v-card class="elevation-12">
+      <v-toolbar flat dark color="#07889B">
+        <v-toolbar-title class="tableTitle"> Contact details </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-layout row>
+          <v-flex xs7>
+            <v-list-tile-content class="infoContainer">
+              <v-list-tile-title>
+                <v-icon>person</v-icon>
+                {{ contact.firstName }} {{ contact.lastName }}
+              </v-list-tile-title>
+              <v-list-tile-sub-title class="emailBar">
+                <v-icon>fa fa-envelope</v-icon>
+                {{ contact.email }}
               </v-list-tile-sub-title>
-              <v-list-tile-sub-title> {{ contact.primaryNumber.numberType }} </v-list-tile-sub-title>
             </v-list-tile-content>
-          </v-list>
-          <v-divider light></v-divider>
-          <v-list class="numbersContainer">
-            <v-list-tile-title> All numbers: </v-list-tile-title>
-            <v-layout v-for="(number, index) in contact.PhoneNumbers" :key="index" row wrap>
-              <v-list two-line>
-                <v-list-tile-content>
-                  <v-list-tile-title><v-icon>phone</v-icon>{{ number.number }}</v-list-tile-title>
-                  <v-list-tile-sub-title>{{ number.numberType }}</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list>
-            </v-layout>
-          </v-list>
-        </v-card-text>
-        <v-card-actions class="buttons">
-          <v-btn @click="editItem" color="#07889B" flat outline>Edit</v-btn>
-          <v-btn @click="navigateTo" flat outline>Return</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+          </v-flex>
+        </v-layout>
+        <v-divider light></v-divider>
+        <v-list two-line>
+          <v-list-tile-content class="primaryNumberContainer">
+            <v-list-tile-title> Primary number: </v-list-tile-title>
+            <v-list-tile-sub-title>
+              <v-icon color="orange">far fa-star</v-icon>
+              {{ contact.primaryNumber.number }}
+            </v-list-tile-sub-title>
+            <v-list-tile-sub-title> {{ contact.primaryNumber.numberType }} </v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list>
+        <v-divider light></v-divider>
+        <v-list class="numbersContainer">
+          <v-list-tile-title> All numbers: </v-list-tile-title>
+          <v-layout v-for="(number, index) in contact.PhoneNumbers" :key="index" row wrap>
+            <v-list two-line>
+              <v-list-tile-content>
+                <v-list-tile-title><v-icon>phone</v-icon>{{ number.number }}</v-list-tile-title>
+                <v-list-tile-sub-title>{{ number.numberType }}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list>
+          </v-layout>
+        </v-list>
+      </v-card-text>
+      <v-card-actions class="buttons">
+        <v-btn @click="editItem" color="#07889B" flat outline>Edit</v-btn>
+        <v-btn @click="navigateTo" flat outline>Return</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -98,6 +96,10 @@ export default {
 </script>
 
 <style scoped>
+.mainContainer {
+  max-width: 30%;
+  margin: auto;
+}
 .infoContainer {
   font-size: 28px;
   padding: 20px;
