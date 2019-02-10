e98dc17ccaf036ebb6e03d6442bbeb3c6d43d9c5
Luka Šarić <lsaric00@fesb.hr>
2019-02-10 16:22:26 +0100

    Deployment edit

--- server/app.js
+++ server/app.js
@@ -5,9 +5,12 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { sequelize } = require('./database');
 const apiRouter = require('./routes/index');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
